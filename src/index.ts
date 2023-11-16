116d05158b80cef73924409f0cc61112177b6431
Vaghela Darsh <darshvaghela253@gmail.com>
2023-11-16 18:13:26 +0530

    mapping added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello welcome to D-Coding!");
+  res.sendFile(path.join(__dirname, "../src/public/index.html"));
 });
 
 app.use("/password", require("./password/controller/passwordController"));
